8f6b75d58b3162ca1a374a1fa1feceb20aaa355a
hiba_khadir <khadirhiba0745@gmail.com>
2025-06-16 01:18:18 +0100

    allowed switching between cycles , add task  feature

--- pomodoro.js
+++ pomodoro.js
@@ -1,39 +1,59 @@
+// App settings
+const settings = {
+  lbInterval: 4,
+  cycleList: [1, 2, 3],
+  autoCheckTsk: 0,
+  autoSwitchTsk: false,
+  autoStrtPomo: false,
+  autoStrtBrk: false
+};
+
 // Timer functionality
 const timer = {
+
+  //params and status of the timer  
   pomoCount: 0,
   currCycle: 0,
-  timerDisp: { min: 25, sec: 0 },
-  totalSec: 25 * 60,
-  over: 0,
+  timerDisp: { min: 0, sec: 3 },
+  totalSec: 3,
   paused: 1,
   countDown: null,
 
+  //ops on timer
   getTimerElmnt() {
     return document.getElementById("timer");
   },
-
+  
+  //display and update
   render() {
     const el = this.getTimerElmnt();
     el.innerHTML = `${String(this.timerDisp.min).padStart(2, '0')} : ${String(this.timerDisp.sec).padStart(2, '0')}`;
 
+    //update the timer
     if (this.totalSec > 0) {
       this.totalSec--;
       this.timerDisp.min = Math.floor(this.totalSec / 60);
       this.timerDisp.sec = this.totalSec % 60;
     } else {
+
       clearInterval(this.countDown);
-      this.over = 1;
-      this.pomoCount++;
-
-      if (this.pomoCount % settings.lbInterval !== 0) {
-        this.currCycle = !this.currCycle; 
-      } else {
-        this.currCycle = 2; // long break
+      //if it's a focus time
+      if (!this.currCycle) {
+        this.pomoCount++; 
+        this.displayCount();       
       }
-
-      this.reset(); // optional auto-switch
-    }
-  },
+      if (this.pomoCount % settings.lbInterval === 0) {
+        this.currCycle = 2; // long break
+      } else {
+        this.currCycle = 1 - this.currCycle;  //toggle bet 1 & 0
+      }
+        this.reset();
+        if ((!this.currCycle && settings.autoStrtPomo )|| (this.currCycle && settings.autoStrtBrk)) {
+            this.unpause();
+        }
+      }
+      console.log(this);
+    },
 
   pause() {
     clearInterval(this.countDown);
@@ -42,8 +62,8 @@ const timer = {
   },
 
   unpause() {
-    this.countDown = setInterval(() => this.render(), 1000);
     this.paused = 0;
+    this.countDown = setInterval(() => this.render(), 1000);
     this.toggleIcon();
   },
 
@@ -70,10 +90,11 @@ const timer = {
     this.pause();
   },
 
-  switchToSB() {
-    this.currCycle = 1;
-    this.reset();
+  displayCount(){
+    let countElmnt = document.getElementById('count');
+    countElmnt.innerHTML  = `#${this.pomoCount + 1}`;
   }
+
 };
 
 // Task management functionality
@@ -101,23 +122,35 @@ const taskManager = {
       if (event.key === "Enter" && taskInput.value !== "") {
         this.taskList.push(taskInput.value);
         console.log(this.taskList);
-        taskInput.value = ""; // clear input
       }
     });
+  },
+
+  save(){
+    let taskInput = document.getElementById("task-input");
+    let pomoNum = document.getElementById('num-put');
+    let controlElmnt = document.getElementById("add-control");
+
+    //temp save of task and it's pomo number
+    if (taskInput.value && pomoNum.value ) {
+        this.taskList.push([taskInput.value , pomoNum.value]);  
+        controlElmnt.style.display = "none";
+        taskInput.value = '';
+        pomoNum.value = '' ;
+    }    
+  },
+  
+  cancel(){
+    alert('progress will be lost');
+    let controlElmnt = document.getElementById("add-control");
+    controlElmnt.style.display = "none";
+    taskInput.value = '';
+    pomoNum.value = '' ; 
   }
+
 };
 
-// App settings
-const settings = {
-  lbInterval: 4,
-  cycleList: [25, 5, 15],
-  autoCheckTsk: 0,
-  autoSwitchTsk: false,
-  autoStrtPomo: false,
-  autoStrtBrk: false
-};
 
-// UI/Page management
 const ui = {
   showSettings() {
     document.getElementById("settings-overlay").style.display = "flex";
